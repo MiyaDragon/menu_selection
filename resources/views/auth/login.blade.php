f543721728f8cdf558b401e06732bc7e5542b101
ryutaro-m <miyagaworld_0912@icloud.com>
2023-06-12 22:01:05 +0900

    ログイン画面更新

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,24 +6,28 @@
 
         @csrf
 
+        <!-- メールアドレス -->
         <div>
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">メールアドレス</label>
-            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" type="email" name="email" :value="old('email')" placeholder="example@example.com" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- パスワード -->
         <div>
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">パスワード</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+            <x-input-label for="password" :value="__('Password')" />
+            <x-text-input id="password" type="password" name="password" placeholder="••••••••" required autocomplete="current-password" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <div class="flex items-start">
-            <a href="{{ route('password.request') }}" class="ml-auto text-sm text-blue-700 hover:underline">パスワードをお忘れの方</a>
+            <a href="{{ route('password.request') }}" class="ml-auto text-sm text-orange-400 hover:underline">パスワードをお忘れの方</a>
         </div>
 
-        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">ログイン</button>
+        <button type="submit" class="w-full text-white bg-orange-300 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center">ログイン</button>
 
         <div class="text-sm font-medium text-gray-500">
-            <a href="{{ route('register') }}" class="text-blue-700 hover:underline">新規登録はこちら</a>
+            <a href="{{ route('register') }}" class="text-orange-400 hover:underline">新規登録はこちら</a>
         </div>
 
     </form>
