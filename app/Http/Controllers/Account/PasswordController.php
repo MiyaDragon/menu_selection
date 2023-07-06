4d68a4dad1960cf277562cd28710b31933c4672f
ryutaro-m <miyagaworld_0912@icloud.com>
2023-07-06 22:18:03 +0900

    パスワード更新処理

--- app/Http/Controllers/Account/PasswordController.php
+++ app/Http/Controllers/Account/PasswordController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PasswordUpdateRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
 
 class PasswordController extends Controller
 {
     /**
-     * Show the form for editing the specified resource.
+     * パスワード更新画面
      */
-    public function edit(Request $request)
+    public function edit(Request $request): View
     {
         return view('accounts.passwords.edit', [
             'user' => $request->user(),
@@ -18,10 +22,14 @@ class PasswordController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * パスワード更新処理
      */
-    public function update(Request $request, string $id)
+    public function update(PasswordUpdateRequest $request): RedirectResponse
     {
-        //
+        $request->user()->update([
+            'password' => Hash::make($request->password),
+        ]);
+
+        return to_route('account.show');
     }
 }
--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AccountTest extends TestCase
@@ -26,5 +27,24 @@ class AccountTest extends TestCase
         $user->refresh();
 
         $this->assertSame('taro', $user->nickname);
+    }
+
+    public function test_password_can_be_updated(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->patch('/account/password', [
+                'current_password' => 'password',
+                'password' => 'new-password',
+                'password_confirmation' => 'new-password',
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/account');
+
+        $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
     }
 }
